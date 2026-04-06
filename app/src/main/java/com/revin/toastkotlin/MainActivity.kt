e41094105bdfac7e552254e324145d8c1a0dc33d
olegspace <olezhka_revin@mail.ru>
2026-04-06 15:41:09 +0700

    Initial commit with first Toast

---
+++ app/src/main/java/com/revin/toastkotlin/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.revin.toastkotlin
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val text = "Пора изучать Kotlin!"
+        val duration = Toast.LENGTH_SHORT
+        Toast.makeText(applicationContext, text, duration).show()
+
+    }
+}
